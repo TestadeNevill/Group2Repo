82cc0fba3e5dbf121457a06ca039f72934b0da4a
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2020-08-29 13:17:55 -0400

    Added a README.me to list or current tasks & database information.

---
+++ README2.md
@@ -0,0 +1,7 @@
+8.29.20
+
+1. Design out all pages.
+   Main Page-Product Pages
+   About/Mission
+   Checkout(Address)
+   Deliver - Google Map
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,12 @@
 -- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS passport_demo;
+
 -- Creates the "todolist" database --
-CREATE DATABASE passport_demo;
+
+CREATE TABLE Products (
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+type varchar(255) NOT NULL,
+sold BOOLEAN DEFAULT false,
+PRIMARY KEY(id),
+FOREIGN KEY(Users.id)
+);
